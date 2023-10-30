5b8c97bc9ab5404762b4ac7bdf3e9cfa073d3f17
KWB <98502147+kimwoobyoung@users.noreply.github.com>
2023-10-30 22:04:26 +0900

    상세보기

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ const express = require('express') // express 라이브러리 사용하겠다는
 const app = express()   // ""
 
 app.use(express.static(__dirname + '/public')); // public 폴더 등록
+app.set('view engine', 'ejs')
+app.use(express.json()) // res.body로 쉽게 꺼내쓸 수 있게 도와주는 코드
+app.use(express.urlencoded({extended: true})) // ''
+const { ObjectId } = require('mongodb') // Objectid() 쓸 수 있게 해줌 
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
@@ -30,4 +34,56 @@ app.get('/news', (req, res) => {
 app.get('/shop', (req, res) => {
     res.send('쇼핑페이지입니다')
 })
+
+app.get('/list', async (req, res) => {
+    let result = await db.collection('post').find().toArray();
+    // console.log(result[0].title);
+    // res.send(result[0].title);
+    res.render('list.ejs', { posts : result })
+});
+
+app.get('/time', async (req, res) => {
+    res.render('time.ejs', { date : new Date() });
+});
+
+app.get('/write', async (req, res) => {
+    res.render('write.ejs');
+});
+
+app.post('/add', async (req, res) => {
+    if (req.body.title == '') {
+        res.send('제목을 적어주세요');
+    } else {
+    // 예외처리와 db에 post에 저장
+        try {
+            await db.collection('post').insertOne({ title: req.body.title, content: req.body.content})
+            res.redirect('list');
+        } catch (err) {
+            console.log(err)
+            res.send('db에러');
+        }
+    }
+});
+
+app.get('/detail/:id', async (req, res) => {
+    try {
+        let result = await db.collection('post').findOne({ _id : new ObjectId(req.params.id) })
+        
+        if (result == null) {
+            res.status(400).send('404 Not Found');
+        } else {
+          res.render('detail.ejs', { result : result })
+        }
+
+    } catch (err) {
+        res.send('예외처리: ', err)
+    }
+    
+});
+
+//수정
+app.get('/edit/:id', async (req, res) => {
+    let result = await db.collection('post').findOne({ __id : new ObjectId(req.params.id)})
+    res.render('edit.ejs')
+});
 
